cc27db8108d0c150e5d0ec991697e0ebef6a1ab5
ObadiahF <obadiahfusco@icloud.com>
2025-09-25 12:45:46 -0700

    Add Layout with nav bar and footer
    Also went and updated the hello route to showcase the changes.

--- src/main/java/com/furnitureStore/controller/helloController.java
+++ src/main/java/com/furnitureStore/controller/helloController.java
@@ -1,21 +1,20 @@
 package com.furnitureStore.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
 
  
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/")
 public class helloController {
 	
 	@GetMapping("/hello")
-	public String sayHello() {
-		return "Hello!";
-	
+	public String sayHello(Model model) {
+		model.addAttribute("title", "Hello Page");
+		return "hello";
 	}
 
 }
